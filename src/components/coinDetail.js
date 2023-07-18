5eab2b68210853541cfc99a59aa5180d50064bff
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-18 23:13:44 +0300

    Add filter for the coin that match the id and render detail of it

--- src/components/coinDetail.js
+++ src/components/coinDetail.js
@@ -1,8 +1,85 @@
-function Coindetail() {
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getCoins } from '../redux/coinSlice';
+
+function CoinDetail() {
+  const coins = useSelector(getCoins);
+  const { id } = useParams();
+  const detailCoin = coins.find((coin) => coin.id === id);
+
   return (
     <div>
-      coins all
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <img src={detailCoin.img} alt={detailCoin.name} />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Symbol:</span>
+              <span>{detailCoin.symbol}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Price:</span>
+              <span>{detailCoin.price}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Volume:</span>
+              <span>{detailCoin.volume}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Market Cap:</span>
+              <span>{detailCoin.marketCap}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Available Supply:</span>
+              <span>{detailCoin.availableSupply}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Total Supply:</span>
+              <span>{detailCoin.totalSupply}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Price Change (1h):</span>
+              <span>{detailCoin.priceChange1h}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Price Change (1d):</span>
+              <span>{detailCoin.priceChange1d}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Price Change (1w):</span>
+              <span>{detailCoin.priceChange1w}</span>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span>Website URL:</span>
+              <span>{detailCoin.websiteUrl}</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
-export default Coindetail;
+
+export default CoinDetail;
