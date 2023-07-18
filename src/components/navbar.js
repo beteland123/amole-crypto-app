5db691e73715a10c9cd50ac7be1ff61d9cde7d59
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-18 12:56:45 +0300

    Add the navs

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import back from '../imags/back.png';
+import micro from '../imags/microphone.png';
+import gear from '../imags/gear.png';
+import '../style/nav.css';
+
+function NAVBAR() {
+  return (
+    <nav className="navbar">
+      <li>
+        <NavLink to="/">
+          <img src={back} className="App-logo" alt="back" />
+        </NavLink>
+      </li>
+      <li><NavLink to="/id" /></li>
+      <li><h1 className="title">coins</h1></li>
+      <li className="icon">
+        <img src={micro} className="App-logo" alt="micro" />
+        <img src={gear} className="App-logo" alt="gear" />
+      </li>
+
+    </nav>
+  );
+}
+export default NAVBAR;
