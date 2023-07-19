32db812cd60862f79dace8e9716064e0c5f2df88
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-19 21:34:06 +0300

    Add detail test

--- src/test/coinSlicer.test.js
+++ src/test/coinSlicer.test.js
@@ -1,7 +1,6 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchcoins,getLoading } from '../redux/coinSlice';
+import { fetchcoins } from '../redux/coinSlice';
 
 const mockStore = configureStore([thunk]);
 
@@ -20,18 +19,18 @@ describe('coinSlice', () => {
     const mockResponse = {
       coins: [
         {
-            id: 'bitcoin',
-            name: 'Bitcoin',
-            symbol: 'BTC',
-            price: 29926.533690045835,
-            volume: 33553053694.82069,
-            marketCap: 581583816449.8502,
-            availableSupply: 19433718,
-            totalSupply: 21000000,
-            priceChange1h:  -0.28,
-            priceChange1d: 0.39,
-            priceChange1w: -2.56,
-            websiteUrl: 'http://www.bitcoin.org',
+          id: 'bitcoin',
+          name: 'Bitcoin',
+          symbol: 'BTC',
+          price: 29926.533690045835,
+          volume: 33553053694.82069,
+          marketCap: 581583816449.8502,
+          availableSupply: 19433718,
+          totalSupply: 21000000,
+          priceChange1h: -0.28,
+          priceChange1d: 0.39,
+          priceChange1w: -2.56,
+          websiteUrl: 'http://www.bitcoin.org',
         },
       ],
     };
@@ -52,18 +51,18 @@ describe('coinSlice', () => {
     const mockResponse = {
       coins: [{}],
     };
-  
+
     jest.spyOn(global, 'fetch').mockResolvedValueOnce({
       json: jest.fn().mockResolvedValue(mockResponse),
     });
-  
+
     await store.dispatch(fetchcoins());
-  
+
     const actions = store.getActions();
     expect(actions[0].type).toEqual(fetchcoins.pending.type);
-    expect(actions[0].payload).toBeUndefined(); 
+    expect(actions[0].payload).toBeUndefined();
   });
-  
+
   it('should handle error on failed response', async () => {
     global.fetch = jest.fn().mockResolvedValue({
       ok: false,
