001efb673e9f6488d68cc3b142b39981f55d9d7a
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-20 22:14:14 +0300

    Add class

--- src/components/coin.js
+++ src/components/coin.js
@@ -13,44 +13,46 @@ function Coin() {
 
   return (
     <div className="coins-cointainer">
-      <h2 className="heading"> All crypto coins</h2>
+      <h2 className="heading lato"> All crypto coins</h2>
       <div className="parent" data-testid="coin-component">
         <div className="column">
-          {coins.filter((_, index) => index % 2 === 0).map((coin) => (
-            <div key={coin.id} className="child">
+          {coins.filter((_, index) => index % 2 === 0).map((coin, index) => (
+            <div key={coin.id} className={`child ${index % 2 === 0 ? 'even' : 'odd'} ${index % 2 === 0 ? 'column-even' : 'column-odd'}`}>
               <Link to={`/coin/${coin.id}`}>
-                <h2>{coin.name}</h2>
-                <img src={coin.img} alt={coin.name} />
-                <span>
-                  symbol :
-                  {' '}
-                  {coin.symbol}
-                </span>
-                <span>
-                  price :
-                  {' '}
-                  {coin.price}
-                </span>
+                <div className="first-row">
+                  <img src={coin.img} alt={coin.name} />
+                  <span className="sans symbol">
+                    {coin.symbol}
+                  </span>
+                  <span className="sans price">
+                    $
+                    {coin.price}
+                  </span>
+                </div>
+                <div className="span">
+                  <h4 className="sans">{coin.name}</h4>
+                </div>
               </Link>
             </div>
           ))}
         </div>
         <div className="column">
-          {coins.filter((_, index) => index % 2 !== 0).map((coin) => (
-            <div key={coin.id} className="child">
+          {coins.filter((_, index) => index % 2 !== 0).map((coin, index) => (
+            <div key={coin.id} className={`child ${index % 2 === 0 ? 'odd' : 'even'} ${index % 2 === 0 ? 'column-odd' : 'column-even'}`}>
               <Link to={`/coin/${coin.id}`}>
-                <h2>{coin.name}</h2>
-                <img src={coin.img} alt={coin.name} />
-                <span>
-                  symbol :
-                  {' '}
-                  {coin.symbol}
-                </span>
-                <span>
-                  price :
-                  {' '}
-                  {coin.price}
-                </span>
+                <div className="first-row">
+                  <img src={coin.img} alt={coin.name} />
+                  <span className="sans symbol">
+                    {coin.symbol}
+                  </span>
+                  <span className="sans price">
+                    $
+                    {coin.price}
+                  </span>
+                </div>
+                <div className="span">
+                  <h4 className="sans">{coin.name}</h4>
+                </div>
               </Link>
             </div>
           ))}
