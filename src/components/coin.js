2cc874f2e9dc4d955abe9b9eaffd6767d1ebcaac
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-21 00:27:53 +0300

    Add the sreaching bar and vationalty

--- src/components/coin.js
+++ src/components/coin.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchcoins, getCoins } from '../redux/coinSlice';
@@ -10,13 +10,30 @@ function Coin() {
   useEffect(() => {
     dispatch(fetchcoins());
   }, [dispatch]);
+  const [search, setSearch] = useState('');
+
+  const filteredCoins = coins.filter((coin) => {
+    const coinName = coin.name.toLowerCase();
+    const searchTerm = search.toLowerCase();
+
+    return coinName.includes(searchTerm);
+  });
 
   return (
     <div className="coins-cointainer">
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
       <h2 className="heading lato"> All crypto coins</h2>
       <div className="parent" data-testid="coin-component">
         <div className="column">
-          {coins.filter((_, index) => index % 2 === 0).map((coin, index) => (
+          {filteredCoins.filter((_, index) => index % 2 === 0).map((coin, index) => (
             <div key={coin.id} className={`child ${index % 2 === 0 ? 'even' : 'odd'} ${index % 2 === 0 ? 'column-even' : 'column-odd'}`}>
               <Link to={`/coin/${coin.id}`}>
                 <div className="first-row">
@@ -37,7 +54,7 @@ function Coin() {
           ))}
         </div>
         <div className="column">
-          {coins.filter((_, index) => index % 2 !== 0).map((coin, index) => (
+          {filteredCoins.filter((_, index) => index % 2 !== 0).map((coin, index) => (
             <div key={coin.id} className={`child ${index % 2 === 0 ? 'odd' : 'even'} ${index % 2 === 0 ? 'column-odd' : 'column-even'}`}>
               <Link to={`/coin/${coin.id}`}>
                 <div className="first-row">
