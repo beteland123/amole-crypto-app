9f5205666f158533fa5e1e1e89784fd21b241b11
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-20 22:13:36 +0300

    add class name and dolar sign

--- src/components/coinDetail.js
+++ src/components/coinDetail.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { getCoins } from '../redux/coinSlice';
+import '../style/coindetail.css';
 
 function CoinDetail() {
   const coins = useSelector(getCoins);
@@ -12,73 +13,97 @@ function CoinDetail() {
       <table>
         <tbody>
           <tr>
-            <td>
+            <td className="tdimg">
               <img src={detailCoin.img} alt={detailCoin.name} />
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Name:</span>
               <span>{detailCoin.name}</span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Symbol:</span>
               <span>{detailCoin.symbol}</span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Price:</span>
-              <span>{detailCoin.price}</span>
+              <span>
+                $
+                {detailCoin.price}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Volume:</span>
-              <span>{detailCoin.volume}</span>
+              <span>
+                $
+                {detailCoin.volume}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Market Cap:</span>
-              <span>{detailCoin.marketCap}</span>
+              <span>
+                $
+                {detailCoin.marketCap}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Available Supply:</span>
-              <span>{detailCoin.availableSupply}</span>
+              <span>
+                $
+                {detailCoin.availableSupply}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Total Supply:</span>
-              <span>{detailCoin.totalSupply}</span>
+              <span>
+                $
+                {detailCoin.totalSupply}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Price Change (1h):</span>
-              <span>{detailCoin.priceChange1h}</span>
+              <span>
+                $
+                {detailCoin.priceChange1h}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Price Change (1d):</span>
-              <span>{detailCoin.priceChange1d}</span>
+              <span>
+                $
+                {detailCoin.priceChange1d}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
+          <tr className="rows">
+            <td className="lists">
               <span>Price Change (1w):</span>
-              <span>{detailCoin.priceChange1w}</span>
+              <span>
+                $
+                {detailCoin.priceChange1w}
+              </span>
             </td>
           </tr>
-          <tr>
-            <td>
-              <span>Website URL:</span>
+          <tr className="rows">
+            <td className="lists">
+              <span>URL:</span>
               <span>{detailCoin.websiteUrl}</span>
             </td>
           </tr>
